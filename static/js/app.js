478a749fe78686a833d20157e2e3f3320cee675a
coryselzer <Personal Computer>
2023-09-21 15:47:36 -0500

    final commit

--- static/js/app.js
+++ static/js/app.js
@@ -28,30 +28,34 @@ function buildMetadata(sample) {
         }
     });
 }
-
+// Create function to build plots and load in url
+// Create variable and call the data
 function buildChart(x) {
     d3.json(url).then(function (data) {
         let samples = data.samples;
+        // Filter the data for object
         let array1 = samples.filter(sampleObject => sampleObject.id == x);
         let result = array1[0];
         console.log(result);
+        // Create variables for charting purposes
         let otu_ids = result.otu_ids;
         let otu_labels = result.otu_labels;
         let sampleValues = result.sample_values;
         console.log(sampleValues);
+        // Sort and slice the sample values to obtain the top 10 OTU_IDs
         let sortedSampleValues = sampleValues.sort((a, b) => b.sampleValues - a.sampleValues);
-        let slicedSampleValues = sortedSampleValues.slice(0, 10);
+        let slicedSampleValues = sortedSampleValues.slice(0, 10).reverse();
+        // Create function within function and create plots
         function buildChart () {
             let trace1 = {
                 x: slicedSampleValues,
-                y: otu_ids,
+                y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
                 text: otu_labels,
                 type: 'bar',
                 orientation: 'h'
             };
             let data = [trace1];
             let layout = {
-                title: 'Sample Values Bar Chart',
                 margin: {
                     l: 100,
                     r: -50,
@@ -66,16 +70,20 @@ function buildChart(x) {
                 y: sampleValues,
                 text: otu_labels,
                 type: 'bubble',
-                mode: 'markers'
+                mode: 'markers',
+                    marker: {
+                        size: sampleValues,
+                        color: otu_ids,
+                        colorscale: "Earth"
+                    }
             };
             let data2 = [trace2];
             let layout2 = {
-                title: 'Sample Values Bubble Chart',
                 margin: {
-                    l: 100,
-                    r: -10,
-                    t: 100,
-                    b: 100
+                    l: 30,
+                    r: 50,
+                    t: 30,
+                    b: 50
                 }
             }
             Plotly.newPlot("bubble", data2, layout2);
@@ -83,12 +91,13 @@ function buildChart(x) {
         buildChart();
 });
 }
-
+// Create function to hold optionChanged
 function optionChanged(params) {
     buildMetadata(params);
     buildChart(params);
 }
 
+// Create function to enable dropdown menu and bring in charts/metadata after selecting new ID
 function init() {
     let dropDown = d3.select("#selDataset");
     d3.json(url).then(function (data) {
